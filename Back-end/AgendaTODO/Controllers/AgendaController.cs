726f34a52d890aad255ba41b74938836324636d5
Amanda Luiza <77518236+AmandaLuiza@users.noreply.github.com>
2022-04-23 08:57:15 -0300

    Add files via upload

---
+++ Back-end/AgendaTODO/Controllers/AgendaController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AgendaTODO.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaTODO.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AgendaController : ControllerBase
+    {
+        private static List<Agenda> agenda = new List<Agenda>();
+        
+
+        [HttpPost]
+        public void AdicionaAgenda([FromBody] Agenda agenda)
+        {
+
+            agenda.Add(agenda);
+
+        }
+
+        [HttpGet()]
+        public IEnumerable<Agenda> RecuperarAgenda()
+        {
+            return agenda;
+        }
+
+        [HttpPut()]
+        public IActionResult AtualizaAgenda(int numero, [FromBody] Agenda agenda)
+       
+        {
+            return NotFound();
+        }
+
+        [HttpDelete()]
+        public IActionResult DeletaAgenda(int numero)
+        {
+            return NotFound();
+        }
+        
+    }
+} 
+    
+
+
+
+
